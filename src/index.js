68a18f692c0075b472dfbb00a2c28fd94858960d
Rafael Pereira <rafaelpereira2.0@outlook.com>
2020-07-31 20:12:54 -0300

    Commit segunda parte

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={()=><div>404 - Not Found</div>} />
+    </Switch>
+  </BrowserRouter>,
+  // <React.StrictMode>
+
+  // </React.StrictMode>,
   document.getElementById('root')
 );
